c3db1927ea4d8ed746d40491031b3e698ca99f27
sky0621 <sky14work@gmail.com>
2017-09-21 00:43:56 +0900

    continue

--- prepare/prepare.go
+++ prepare/prepare.go
@@ -5,6 +5,9 @@ import (
 	"io/ioutil"
 	"os"
 
+	"strings"
+
+	"github.com/ChimeraCoder/gojson"
 	"github.com/line/line-bot-sdk-go/linebot"
 	"github.com/mcuadros/go-jsonschema-generator"
 	"github.com/spf13/cobra"
@@ -14,10 +17,19 @@ func MakeCommand() func(cmd *cobra.Command, args []string) {
 	return func(cmd *cobra.Command, args []string) {
 		fmt.Println("prepare called!!!")
 		sd := &jsonschema.Document{}
-		sd.Read(&struct {
-			Type linebot.MessageType `json:"type"`
-			Text string              `json:"text"`
-		}{})
+		//sd.Read(&struct {
+		//	Type linebot.MessageType `json:"type"`
+		//	Text string              `json:"text"`
+		//}{})
+		tm := linebot.NewTextMessage("dummy")
+		ba, _ := tm.MarshalJSON()
+		fmt.Println(string(ba))
+		js, err := gojson.ParseJson(strings.NewReader(string(ba)))
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(js)
+		sd.Read(&linebot.Event{})
 		ioutil.WriteFile("schemajson/reply-textmessage.json", []byte(sd.String()), os.ModePerm)
 	}
 }
